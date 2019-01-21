534b0346df2cccc14e70b7c8df77e37894e1a8a2
bjiang <bjiang@pharmamedica.com>
2019-01-21 14:02:17 -0500

    Merge the two users' changes

---
+++ MultiUserDbChanges/Migrations/201901211900171_Merge.cs
@@ -0,0 +1,16 @@
+namespace MultiUserDbChanges.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Merge : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
